01f1c65d5b03ae7926b87842ea83dcde67410bb4
vusalaxndzde <vusal.axndzde@gmail.com>
2023-04-07 23:24:02 +0400

    properties

--- 01-spring-boot-overview/06-properties-demo/src/main/java/com/vusalaxndzde/springboot/demo/mycoolapp/rest/HelloWorldController.java
+++ 01-spring-boot-overview/06-properties-demo/src/main/java/com/vusalaxndzde/springboot/demo/mycoolapp/rest/HelloWorldController.java
@@ -1,10 +1,22 @@
 package com.vusalaxndzde.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+    @Value("${player.name}")
+    private String playerName;
+    
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("teaminfo")
+    public String getTeamInfo() {
+        return "Team: " + teamName + ", Player: " + playerName;
+    }
 
     @GetMapping("/")
     public String sayHello() {
