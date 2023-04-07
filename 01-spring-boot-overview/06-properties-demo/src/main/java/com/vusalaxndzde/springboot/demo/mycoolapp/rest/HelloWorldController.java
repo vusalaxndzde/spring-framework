c8333c0b14fb93565a9e4749ea2609e6392eefaf
vusalaxndzde <vusal.axndzde@gmail.com>
2023-04-07 23:16:15 +0400

    properties

---
+++ 01-spring-boot-overview/06-properties-demo/src/main/java/com/vusalaxndzde/springboot/demo/mycoolapp/rest/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.vusalaxndzde.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World";
+    }
+
+    @GetMapping("/workout")
+    public String workout() {
+        return "Run a hard 5k!";
+    }
+    
+}
