4a172d43e945e3c17d9deb52fa0958a012c34bac
vusalaxndzde <vusal.axndzde@gmail.com>
2023-04-09 22:29:26 +0400

    changes

---
+++ 02-spring-boot-spring-core/07-bean-scopes/src/main/java/com/vusalaxndzde/springcoredemo/rest/DemoController.java
@@ -0,0 +1,35 @@
+package com.vusalaxndzde.springcoredemo.rest;
+
+import com.vusalaxndzde.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private Coach coach;
+    private Coach anotherCoach;
+
+    @Autowired
+    public DemoController(
+            @Qualifier("cricketCoach") Coach coach,
+            @Qualifier("cricketCoach") Coach anotherCoach
+    ) {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+        this.coach = coach;
+        this.anotherCoach = anotherCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return coach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Comparing beans: coach == anotherCoach, " + (coach == anotherCoach);
+    }
+
+}
