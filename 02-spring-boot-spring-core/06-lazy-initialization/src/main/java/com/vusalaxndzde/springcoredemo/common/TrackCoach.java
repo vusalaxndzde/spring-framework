bd63dac5f78196a4cd9aa3134e6340fa918c7536
vusalaxndzde <vusal.axndzde@gmail.com>
2023-04-09 21:54:42 +0400

    changes

---
+++ 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/vusalaxndzde/springcoredemo/common/TrackCoach.java
@@ -0,0 +1,19 @@
+package com.vusalaxndzde.springcoredemo.common;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+@Lazy
+public class TrackCoach implements Coach {
+
+    public TrackCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "track coach";
+    }
+
+}
