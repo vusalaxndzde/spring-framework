228b1b3d2536829fcb179de73982376aa17e5ae4
vusalaxndzde <vusal.axndzde@gmail.com>
2023-04-09 16:43:10 +0400

    changes

--- 02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/vusalaxndzde/springcoredemo/CricketCoach.java
+++ 02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/vusalaxndzde/springcoredemo/CricketCoach.java
@@ -7,7 +7,7 @@ public class CricketCoach implements Coach {
 
     @Override
     public String getDailyWorkout() {
-        return "Practice fast bowling for 15 minutes";
+        return "Practice fast bowling for 15 minutes!!!";
     }
 
 }
