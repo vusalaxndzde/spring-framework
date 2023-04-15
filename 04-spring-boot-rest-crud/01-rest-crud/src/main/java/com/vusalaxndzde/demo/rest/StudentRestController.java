80d08ef04eafa5b348017905acc23537ea6cea26
vusalaxndzde <vusal.axndzde@gmail.com>
2023-04-15 20:39:53 +0400

    changes

--- 04-spring-boot-rest-crud/01-rest-crud/src/main/java/com/vusalaxndzde/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-rest-crud/src/main/java/com/vusalaxndzde/demo/rest/StudentRestController.java
@@ -2,10 +2,9 @@ package com.vusalaxndzde.demo.rest;
 
 import com.vusalaxndzde.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,7 +31,23 @@ public class StudentRestController {
 
     @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId) {
+
+        if ((studentId > students.size()) || (studentId < 0)) {
+            throw new StudentNotFoundException("Student not found: " + studentId);
+        }
+
         return students.get(studentId);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+        StudentErrorResponse errorResponse = new StudentErrorResponse();
+
+        errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
+        errorResponse.setMessage(exc.getMessage());
+        errorResponse.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
 }
