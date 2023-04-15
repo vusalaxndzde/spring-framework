94d3b63748aad4212aa5122c61b16494b1d75ede
vusalaxndzde <vusal.axndzde@gmail.com>
2023-04-15 19:16:35 +0400

    changes

--- 04-spring-boot-rest-crud/01-rest-crud/src/main/java/com/vusalaxndzde/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-rest-crud/src/main/java/com/vusalaxndzde/demo/rest/StudentRestController.java
@@ -3,6 +3,7 @@ package com.vusalaxndzde.demo.rest;
 import com.vusalaxndzde.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +28,11 @@ public class StudentRestController {
     @GetMapping("/students")
     public List<Student> getStudents() {
         return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return students.get(studentId);
     }
 
 }
