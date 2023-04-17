aebb3b10ab7be743ee24ddd4d7b4dd077a8e4a8e
vusalaxndzde <vusal.axndzde@gmail.com>
2023-04-18 02:24:39 +0400

    changes

--- 04-spring-boot-rest-crud/04-rest-crud-employee-with-spring-data-rest/src/main/java/com/vusalaxndzde/springboot/cruddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/04-rest-crud-employee-with-spring-data-rest/src/main/java/com/vusalaxndzde/springboot/cruddemo/dao/EmployeeRepository.java
@@ -3,6 +3,7 @@ package com.vusalaxndzde.springboot.cruddemo.dao;
 import com.vusalaxndzde.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 
