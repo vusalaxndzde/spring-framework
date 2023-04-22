c79801457e2ecd08058234cdd10321b3568a5179
vusalaxndzde <vusal.axndzde@gmail.com>
2023-04-22 23:12:45 +0400

    changes

---
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-hello-world/src/main/java/com/vusalaxndzde/thymeleafdemo/controller/DemoController.java
@@ -0,0 +1,18 @@
+package com.vusalaxndzde.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Date;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String sayHello(Model model) {
+        model.addAttribute("theDate", new Date());
+        return "helloworld";
+    }
+
+}
