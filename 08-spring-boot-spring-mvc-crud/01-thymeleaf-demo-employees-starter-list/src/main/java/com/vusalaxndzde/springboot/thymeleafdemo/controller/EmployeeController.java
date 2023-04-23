25aca760edba73c6e88a66be40f05b16c62647b9
vusalaxndzde <vusal.axndzde@gmail.com>
2023-04-23 19:06:02 +0400

    changes

--- 08-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/vusalaxndzde/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 08-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/vusalaxndzde/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -1,47 +1,25 @@
 package com.vusalaxndzde.springboot.thymeleafdemo.controller;
 
-import com.vusalaxndzde.springboot.thymeleafdemo.entity.Employee;
-import jakarta.annotation.PostConstruct;
+import com.vusalaxndzde.springboot.thymeleafdemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
 
-	// load employee data
+	private final EmployeeService employeeService;
 
-	private List<Employee> theEmployees;
-
-	@PostConstruct
-	private void loadData() {
-
-		// create employees
-		Employee emp1 = new Employee("Leslie", "Andrews", "leslie@luv2code.com");
-		Employee emp2 = new Employee("Emma", "Baumgarten", "emma@luv2code.com");
-		Employee emp3 = new Employee("Avani", "Gupta", "avani@luv2code.com");
-
-		// create the list
-		theEmployees = new ArrayList<>();
-
-		// add to the list
-		theEmployees.add(emp1);
-		theEmployees.add(emp2);
-		theEmployees.add(emp3);
+	public EmployeeController(EmployeeService employeeService) {
+		this.employeeService = employeeService;
 	}
-
-	// add mapping for "/list"
 
 	@GetMapping("/list")
 	public String listEmployees(Model theModel) {
-
-		theModel.addAttribute("employees", theEmployees);
-
+		theModel.addAttribute("employees", employeeService.findAll());
 		return "list-employees";
 	}
+
 }
