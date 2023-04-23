9e45c71d0f7e3f9277d405216119b53c1b4699a5
vusalaxndzde <vusal.axndzde@gmail.com>
2023-04-23 18:56:07 +0400

    changes

--- 08-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-starter-list/sql-scripts/employee-directory.sql
+++ 08-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-starter-list/sql-scripts/employee-directory.sql
@@ -20,9 +20,9 @@ CREATE TABLE `employee` (
 --
 
 INSERT INTO `employee` VALUES 
-	(1,'Leslie','Andrews','leslie@luv2code.com'),
-	(2,'Emma','Baumgarten','emma@luv2code.com'),
-	(3,'Avani','Gupta','avani@luv2code.com'),
-	(4,'Yuri','Petrov','yuri@luv2code.com'),
-	(5,'Juan','Vega','juan@luv2code.com');
+	(1,'Leslie','Andrews','leslie@gmail.com'),
+	(2,'Emma','Baumgarten','emma@gmail.com'),
+	(3,'Avani','Gupta','avani@gmail.com'),
+	(4,'Yuri','Petrov','yuri@gmail.com'),
+	(5,'Juan','Vega','juan@gmail.com');
 
