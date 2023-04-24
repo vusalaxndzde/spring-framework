d428a2f2e9e8ca5d1894f4f90d43d4dbdb07b56f
vusalaxndzde <vusal.axndzde@gmail.com>
2023-04-24 18:29:18 +0400

    changes

--- 08-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/vusalaxndzde/springboot/thymeleafdemo/controller/DemoController.java
+++ 08-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/vusalaxndzde/springboot/thymeleafdemo/controller/DemoController.java
@@ -9,9 +9,7 @@ public class DemoController {
 
 	@GetMapping("/hello")
 	public String sayHello(Model theModel) {
-		
 		theModel.addAttribute("theDate", new java.util.Date());
-		
 		return "helloworld";
 	}
 
