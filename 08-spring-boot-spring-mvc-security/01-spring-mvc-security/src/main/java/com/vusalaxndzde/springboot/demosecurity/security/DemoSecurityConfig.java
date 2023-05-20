a08c20d8270b37a4ef6896b8ed4fa0cf97853292
vusalaxndzde <vusal.axndzde@gmail.com>
2023-05-20 22:41:09 +0400

    changes

--- 08-spring-boot-spring-mvc-security/01-spring-mvc-security/src/main/java/com/vusalaxndzde/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/01-spring-mvc-security/src/main/java/com/vusalaxndzde/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -41,6 +41,9 @@ public class DemoSecurityConfig {
                 .authorizeHttpRequests(configurer ->
                         configurer
                                 .requestMatchers("/css/**").permitAll()
+                                .requestMatchers("/").hasRole("EMPLOYEE")
+                                .requestMatchers("/leaders").hasRole("MANAGER")
+                                .requestMatchers("/systems").hasRole("ADMIN")
                                 .anyRequest().authenticated())
 
                 .formLogin(form ->
